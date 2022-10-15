eedde8cabbb7760c2df4255ef42f2210ed26d7d8
Greg Lucas <greg.m.lucas@gmail.com>
2022-10-15 17:34:45 -0600

    Windows: Change installation dir paths and debugging

--- pymsis/__init__.py
+++ pymsis/__init__.py
@@ -7,18 +7,12 @@ __version__ = "0.5.0"
 # The libraries are located relative to this init file.
 if os.name == "nt":
     pymsis_dir = None
-    pymsis_dir_libs = None
-    try:
-        # add folder to Windows DLL search paths
-        pymsis_dir = Path(__file__).resolve().parent
-        pymsis_dir_libs = pymsis_dir / ".libs"
-        # This was added in Python 3.8, so we can default to this
-        # once we only support 3.8+
-        os.add_dll_directory(pymsis_dir)
-        # Additionally, we need the .libs directory which has gfortran
-        os.add_dll_directory(pymsis_dir_libs)
+    # add folder to Windows DLL search paths
+    pymsis_dir = Path(__file__).resolve().parent
+    print("INIT DIR:", pymsis_dir)
+    for p in pymsis_dir.iterdir():
+        print(p)
+    os.add_dll_directory(pymsis_dir)
 
-        os.environ["PATH"] = f"{pymsis_dir};{pymsis_dir_libs};" f"{os.environ['PATH']}"
-    except Exception:
-        pass
-    del pymsis_dir, pymsis_dir_libs
+    os.environ["PATH"] = f"{pymsis_dir};" f"{os.environ['PATH']}"
+    del pymsis_dir
